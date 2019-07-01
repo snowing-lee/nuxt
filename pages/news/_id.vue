383deb6aa52afd60c7d07026dbb03876c40448ab
snow-lee <2470374084@qq.com>
2019-07-01 11:00:50 +0800

    动态路由  和  参数效验

---
+++ pages/news/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>News-content</h2>
+
+    <p>NewsId:{{$route.params.id}}</p>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  //  _xxx.vue   (以下划线开始)为动态路由固定写法
+export default {
+    name: "_id",
+    validate({params}){
+      // 效验参数是数字,返回true正常进入动态页面,返回false进入nuxt准备的404页面
+      return  /^\d+$/.test(params.id);
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -7,6 +7,9 @@
     <ul>
       <li> <nuxt-link :to="{name:'index'}">Home</nuxt-link> </li>
       <li> <a href="/">Home</a></li>
+      <li> <a href="/news/1">News1</a></li>
+      <li> <a href="/news/2">News2</a></li>
+      <li> <a href="/news/s">News3</a></li>
     </ul>
 
 
