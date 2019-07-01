bcf10054fae86e84871822f45f36bbd32f9668d7
snow-lee <2470374084@qq.com>
2019-07-01 11:48:12 +0800

    配置404,500 页面

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <!-- 配置404页面: 在layouts/创建 error.vue  无需其他配置
+          当发生错误时会自动加载此页面-->
+
+    <h2 v-if="error.statusCode==404">
+      404了.......
+    </h2>
+    <h2 v-else>
+      500了.......
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  props: ['error']
+}
+</script>
+
+<style scoped>
+
+</style>
